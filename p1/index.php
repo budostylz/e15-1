241f5f7a76f9fce03702757fcc8acd486fd537ec
Susan Buck <susan@susanbuck.net>
2020-02-04 18:50:44 -0500

    Initial start on p1 functions

--- p1/index.php
+++ p1/index.php
@@ -1,4 +1,20 @@
-<?php 
+<?php
+
+function isPalindrome($inputString)
+{
+}
+
+function isBigWord($inputString)
+{
+    if (strlen($inputString) > 7) {
+        return 'Yes';
+    } else {
+        return 'No';
+    }
+}
+
+$result1 = isBigWord('cat');
+$result2 = isBigWord('mississippi');
 
 
 require 'index-view.php';
